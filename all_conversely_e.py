819e2c8217fb9a4b967e94f5b2cf35a0947a5387
netzen86 <netzen0su@gmail.com>
2022-07-17 02:11:14 +0500

    задания со связаным списком

---
+++ all_conversely_e.py
@@ -0,0 +1,53 @@
+# ! change LOCAL to False before submitting !
+# set LOCAL to True for local testing
+
+LOCAL = True
+
+if LOCAL:
+    class DoubleConnectedNode:
+        def __init__(self, value, next=None, prev=None):
+            self.value = value
+            self.next = next
+            self.prev = prev
+
+
+def solution(node):
+    revers_node = DoubleConnectedNode("node0")
+    while node:
+        prev = node.prev
+        revers_node.prev = node.next
+        revers_node.next = prev
+        node = node.next
+        if revers_node:
+            print(revers_node.value)
+        print(revers_node)
+    return revers_node
+
+
+def test():
+    node3 = DoubleConnectedNode("node3")
+    node2 = DoubleConnectedNode("node2")
+    node1 = DoubleConnectedNode("node1")
+    node0 = DoubleConnectedNode("node0")
+
+    node0.next = node1
+
+    node1.prev = node0
+    node1.next = node2
+
+    node2.prev = node1
+    node2.next = node3
+
+    node3.prev = node2
+    new_head = solution(node0)
+    # assert new_head is node3
+    # assert node3.next is node2
+    # assert node2.next is node1
+    # assert node2.prev is node3
+    # assert node1.next is node0
+    # assert node1.prev is node2
+    # assert node0.prev is node1
+
+
+if __name__ == '__main__':
+    test()
