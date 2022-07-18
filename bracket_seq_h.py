bd8cfa98265115799c9467dd7842d55ae7c11f69
netzen86 <netzen0su@gmail.com>
2022-07-19 01:44:27 +0500

    последовательность скобок

---
+++ bracket_seq_h.py
@@ -0,0 +1,22 @@
+        
+def is_correct_bracket_seq(seq):
+    stack = []
+    brackets = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    }
+    for bracket in seq:
+        if bracket in ['(', '{', '[']:
+            stack.append(bracket)
+        elif bracket in [')', '}', ']']:
+            if not stack:
+                return False
+            popped = stack.pop()
+            if brackets[popped] != bracket:
+                return False
+    return stack == []
+
+if __name__ == '__main__':
+    seq = input()
+    print(is_correct_bracket_seq(seq))
