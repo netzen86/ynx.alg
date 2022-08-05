8ca9ff180c5eef96c0886c76672588e4a082dfdb
netzen86 <netzen0su@gmail.com>
2022-08-06 01:30:30 +0500

    поиск в сломанном массиве

--- search_broke_array_a.py
+++ search_broke_array_a.py
@@ -1,17 +1,23 @@
+# id посылки 69544838
 
 def find_pivot(arr, low, high):
     '''Функция для поиска точки сдвига'''
+    # если указатели встретились массив не сдвинут выходим из рекурсии
     if high < low:
         return -1
+    # найдена точка сдвига
     if high == low:
         return low
     mid = int((low + high)/2)
+    # ищем точки сдвига сравнивая элементы
     if mid < high and arr[mid] > arr[mid + 1]:
         return mid
     if mid > low and arr[mid] < arr[mid - 1]:
         return (mid-1)
+    # если левая граница больше середины то сдвигаем середину влево
     if arr[low] >= arr[mid]:
         return find_pivot(arr, low, mid-1)
+    # сдвигаем середину вправо
     return find_pivot(arr, mid + 1, high)
 
 
@@ -31,10 +37,13 @@ def broken_search(nums, target) -> int:
     '''Функция поиска элемента в сдвинутом массиве'''
     n = len(nums)
     pivot = find_pivot(nums, 0, n-1)
+    # если массив не сдвинут просто ищем бинарным поиском
     if pivot == -1:
         return binary_search(nums, 0, n-1, target)
+    # если точка сдвдига это искомый элемент сразу его возвращаем
     if nums[pivot] == target:
         return pivot
+    # поиск в сдвинутом массиве вызовом функции рекурсивно
     if nums[0] <= target:
         return binary_search(nums, 0, pivot-1, target)
     return binary_search(nums, pivot + 1, n-1, target)
